dcd4758158619d5837aca94d421e71db57831915
Mayana <mayana.clesia@gmail.com>
2023-10-29 01:01:50 -0300

    Criando classes e atributos

--- core/models.py
+++ core/models.py
@@ -1,3 +1,25 @@
+from os import link
 from django.db import models
 
-# Create your models here.
+class Narrativa(models.Model):
+    titulo = models.CharField('Titulo',max_length=100)
+    descricao = models.CharField('Descrição',max_length=150)
+    foto = models.ImageField('Foto')
+    autor = models.CharField('Autor', max_length=50)
+    anexo = models.CharField('Anexo')
+    link = models.CharField('Link')
+
+    def _str_(self):
+        return "{} ({})".format(self.titulo, self.descricao, 
+                                self.foto, self.autor, self.anexo, self.link)
+    
+class Indicadores_narrativa(models.Model):
+    qtd_Personagens_total = models.IntegerField('Quantidade de Personagens Total ')
+    qtd_Personagens_reais = models.IntegerField('Quantidade de Personagens Reais ')
+    qtd_Personagens_imaginarios = models.IntegerField('Quantidade de Personagens Imaginarios')
+    qtd_Personagens_criancas = models.IntegerField('Quantidade de Personagens Crianças ')
+    qtd_Personagens_idosos = models.IntegerField('Quantidade de Personagens Idosos')
+
+
+
+
