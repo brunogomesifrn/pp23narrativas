9acb248a014571d10ca887469b7fc2912ad5efa1
Bruno Gomes de Araujo <2729795@ifrn.local>
2023-11-08 16:31:40 -0300

    Script do BD

---
+++ temp/banco_dados/script.sql
@@ -0,0 +1,9 @@
+insert into core_tipo_narrativa (id, nome) values (1, 'Lenda');
+insert into core_tipo_narrativa (id, nome) values (2, 'Conto');
+insert into core_tipo_narrativa (id, nome) values (3, 'História');
+
+insert into core_estilo_narrativa (id, nome) values (1, 'Ação');
+insert into core_estilo_narrativa (id, nome) values (2, 'Aventura');
+
+insert into core_narrativa (id, titulo, descricao, autor, tipo_narrativa_id) values (1, 'Saci', 'Lenda do Saci', 'Desconhecido', 1);
+
