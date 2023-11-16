bd79c83ec27eb6da7d3649894ed8a52cca283d1d
Bruno Gomes <bruno.gomes@ifrn.edu.br>
2023-11-16 15:16:48 -0300

    Narrativas

--- core/views.py
+++ core/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Indicadores_Narrativa
+import csv 
 
-# Create your views here.
+
+def narrativas(request):
+    indicadores = Indicadores_Narrativa.objects.all()
+    lista_narrativas = []
+    dicionario = {}
+    chave = ''
+    valores = ''
+    for indicador in indicadores:
+        dicionario[indicador.narrativa.titulo] = [indicador.qtd_personagens_reais, indicador.qtd_personagens_imaginarios]
+        lista_narrativas.append(dicionario)
+        with open('Narrativas.csv', 'w') as csvfile: 
+            writer = csv.DictWriter(csvfile) 
+            writer.writeheader() 
+            writer.writerows(lista_narrativas) 
+    contexto = {
+        'indicadores': indicadores
+    }
+    return render(request, 'narrativas.html', contexto)
--- temp/banco_dados/script.sql
+++ temp/banco_dados/script.sql
@@ -5,45 +5,55 @@ insert into core_tipo_narrativa (id, nome) values (3, 'História');
 insert into core_estilo_narrativa (id, nome) values (1, 'Ação');
 insert into core_estilo_narrativa (id, nome) values (2, 'Aventura');
 insert into core_estilo_narrativa (id, nome) values (3, 'Terror');
+insert into core_estilo_narrativa (id, nome) values (4, 'Humor');
 
 insert into core_narrativa (id, titulo, descricao, autor, tipo_narrativa_id) values (1, 'Saci', 'Lenda do Saci', 'Desconhecido', 1);
-insert into core_narrativa (id, titulo, descricao, autor, tipo_narrativa_id) values (2, 'Cumadre Fulozinha', 'Desconhecido', 3);
-insert into core_narrativa (id, titulo, descricao, autor, tipo_narrativa_id) values (3, 'Botijas de Ouro', 'Desconhecido', 3);
-insert into core_narrativa (id, titulo, descricao, autor, tipo_narrativa_id) values (4, 'Prossissao das almas', 'Desconhecido', 3);
-insert into core_narrativa (id, titulo, descricao, autor, tipo_narrativa_id) values (5, 'Batatao', 'Desconhecido', 3);
-insert into core_narrativa (id, titulo, descricao, autor, tipo_narrativa_id) values (6, 'Pesador', 'Desconhecido', 3);
-insert into core_narrativa (id, titulo, descricao, autor, tipo_narrativa_id) values (7, 'Corpo Seco', 'Desconhecido', 3);
-insert into core_narrativa (id, titulo, descricao, autor, tipo_narrativa_id) values (8, 'Mula sem cabeca', 'Desconhecido', 1);
-insert into core_narrativa (id, titulo, descricao, autor, tipo_narrativa_id) values (9, 'Vitoria-Regia', 'Desconhecido', 2);
-insert into core_narrativa (id, titulo, descricao, autor, tipo_narrativa_id) values (10, 'Lobsomen', 'Desconhecido', 3);
-insert into core_narrativa (id, titulo, descricao, autor, tipo_narrativa_id) values (11, 'Boto Cor de Rosa', 'Desconhecido', 2);
-insert into core_narrativa (id, titulo, descricao, autor, tipo_narrativa_id) values (12, 'Iara', 'Desconhecido', 2);
+insert into core_narrativa (id, titulo, descricao, autor, tipo_narrativa_id) values (2, 'Cumadre Fulozinha', '', 'Desconhecido', 3);
+insert into core_narrativa (id, titulo, descricao, autor, tipo_narrativa_id) values (3, 'Botijas de Ouro', '', 'Desconhecido', 3);
+insert into core_narrativa (id, titulo, descricao, autor, tipo_narrativa_id) values (4, 'Prossissao das almas', '', 'Desconhecido', 3);
+insert into core_narrativa (id, titulo, descricao, autor, tipo_narrativa_id) values (5, 'Batatao', '', 'Desconhecido', 3);
+insert into core_narrativa (id, titulo, descricao, autor, tipo_narrativa_id) values (6, 'Pesador', '', 'Desconhecido', 3);
+insert into core_narrativa (id, titulo, descricao, autor, tipo_narrativa_id) values (7, 'Corpo Seco', '', 'Desconhecido', 3);
+insert into core_narrativa (id, titulo, descricao, autor, tipo_narrativa_id) values (8, 'Mula sem cabeca', '', 'Desconhecido', 1);
+insert into core_narrativa (id, titulo, descricao, autor, tipo_narrativa_id) values (9, 'Vitoria-Regia', '', 'Desconhecido', 2);
+insert into core_narrativa (id, titulo, descricao, autor, tipo_narrativa_id) values (10, 'Lobsomen', '', 'Desconhecido', 3);
+insert into core_narrativa (id, titulo, descricao, autor, tipo_narrativa_id) values (11, 'Boto Cor de Rosa', '', 'Desconhecido', 2);
+insert into core_narrativa (id, titulo, descricao, autor, tipo_narrativa_id) values (12, 'Iara', '', 'Desconhecido', 2);
 
 insert into core_local_narrativa (id, nome) values (1, 'Rural');
 insert into core_local_narrativa (id, nome) values (2, 'Urbano');
 insert into core_local_narrativa (id, nome) values (3, 'Litoral');
 insert into core_local_narrativa (id, nome) values (4, 'Mangue');
+insert into core_local_narrativa (id, nome) values (5, 'Floresta');
 
-insert into core_turno (id, nome) values (1, 'Dia');
-insert into core_turno (id, nome) values (2, 'Noite');
+insert into core_turno_narativa (id, nome) values (1, 'Dia');
+insert into core_turno_narativa (id, nome) values (2, 'Noite');
 
-insert into core_perido_narrativa (id, nome) values (1, '');
+insert into core_periodo_narrativa (id, nome) values (1, 'Indefinido');
 insert into core_periodo_narrativa (id, nome) values (2, '');
 
-insert into cor_publico_destino (id, nome) values (1, 'Criança');
-insert into cor_publico_destino (id, nome) values (2, 'Adulto');
-insert into cor_publico_destino (id, nome) values (3, 'Idoso');
+insert into core_publico_destino (id, nome) values (1, 'Criança');
+insert into core_publico_destino (id, nome) values (2, 'Adulto');
+insert into core_publico_destino (id, nome) values (3, 'Idoso');
+insert into core_publico_destino (id, nome) values (4, 'Todos');
 
-insert into cor_indicadores_narrativa (id, qtd_personagens, narrativa_id, periodo_narrativa_id) values (1, 'Personagens Total');
-insert into cor_indicadores_narrativa (id, qtd_personagens, narrativa_id, periodo_narrativa_id) values (2, 'Personagens Reais');
-insert into cor_indicadores_narrativa (id, qtd_personagens, narrativa_id, periodo_narrativa_id) values (3, 'Personagens Imaginarios');
-insert into cor_indicadores_narrativa (id, qtd_personagens, narrativa_id, periodo_narrativa_id) values (4, 'Personagens Crianças');
-insert into cor_indicadores_narrativa (id, qtd_personagens, narrativa_id, periodo_narrativa_id) values (5, 'Personagens Adultos');
-insert into cor_indicadores_narrativa (id, qtd_personagens, narrativa_id, periodo_narrativa_id) values (6, 'Personagens Idoso');
+/* Saci */
+insert into core_indicadores_narrativa (id, qtd_personagens_reais, 
+qtd_personagens_imaginarios, qtd_personagens_criancas, qtd_personagens_adultos, 
+qtd_personagens_idosos, qtd_personagens_animais, narrativa_id, periodo_id) 
+values (1, 0, 1, 0, 1, 0, 0, 1, 1);
 
+insert into core_narrativa_estilo_narrativa 
+(id, narrativa_id, estilo_narrativa_id) values (1,1,1);
 
+insert into core_narrativa_estilo_narrativa 
+(id, narrativa_id, estilo_narrativa_id) values (2,1,4);
 
+insert into core_indicadores_narrativa_locais
+(id, indicadores_narrativa_id, local_narrativa_id) values (1, 1, 5)
 
+insert into core_indicadores_narrativa_turnos
+(id, indicadores_narrativa_id, turno_narativa_id) values (1, 1, 1)
 
-
-
+insert into core_indicadores_narrativa_publicos
+(id, indicadores_narrativa_id, publico_destino_id) values (1, 1, 4)
