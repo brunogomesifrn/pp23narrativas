c6d30aacd13b4c2cbbfe70ac6c4ccc63a8747c20
J-Jordson <joasjordson19@gmail.com>
2024-12-18 10:04:18 -0300

    Modularização1

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, login, sobre
 
 urlpatterns = [
 
     path('', home, name='home'),
+    path('login/', login, name='login'),
+    path('sobre/', sobre, name='sobre'),
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render, redirect
 
 def home(request):
     return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def sobre(request):
+    return render(request, 'sobre.html')
